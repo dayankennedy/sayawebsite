d7f890e10f3508ccc85009d88876d2bd6c300338
dayankennedy <dayankennedyspiid@gmail.com>
2023-03-17 01:31:26 +0300

    configuring whitenoise

--- sayaWeb/settings.py
+++ sayaWeb/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sayaAPP'
+    'whiteNoise'
 ]
 
 MIDDLEWARE = [
@@ -130,12 +131,22 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, '/static/')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+STORAGES = {
+    "staticfiles": {
+    "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+
+}
+
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # media root(image, videos)
 
