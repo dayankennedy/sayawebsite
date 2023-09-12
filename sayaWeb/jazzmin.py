8ae1739b60f2db60a425ff109d9059b233906c10
dayankennedy <dayankennedyspiid@gmail.com>
2023-09-12 13:04:06 +0300

    jazzmin

--- sayaWeb/jazzmin.py
+++ sayaWeb/jazzmin.py
@@ -12,13 +12,13 @@ JAZZMIN_SETTINGS = {
     "site_logo": "images/header-logo.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": True,
+    "login_logo": False,
 
     # Logo to use for login form in dark themes (defaults to login_logo)
-    "login_logo_dark": True,
+    "login_logo_dark": False,
 
     # CSS classes that are applied to the logo above
-    "site_logo_classes": "img-circle",
+    "site_logo_classes": "img-square",
 
     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
     "site_icon": True,
